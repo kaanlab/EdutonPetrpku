00e7b9c6d81e8785cc97141ad4c8b429c27dc3b5
Anton Kantyshev <kaan@live.ru>
2021-10-21 17:15:10 +0300

    Add migration for jwt refresh token

--- EdutonPetrpku.Data/Migrations/20211021134041_AddRefreshToken.Designer.cs
+++ EdutonPetrpku.Data/Migrations/20211021134041_AddRefreshToken.Designer.cs
@@ -4,14 +4,16 @@ using EdutonPetrpku.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EdutonPetrpku.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211021134041_AddRefreshToken")]
+    partial class AddRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EdutonPetrpku.Data/Migrations/20211021134041_AddRefreshToken.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EdutonPetrpku.Data.Migrations
+{
+    public partial class AddRefreshToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Nationalities_AppUserId",
+                table: "Nationalities");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nationalities_AppUserId",
+                table: "Nationalities",
+                column: "AppUserId",
+                unique: true,
+                filter: "[AppUserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Nationalities_AppUserId",
+                table: "Nationalities");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nationalities_AppUserId",
+                table: "Nationalities",
+                column: "AppUserId");
+        }
+    }
+}
